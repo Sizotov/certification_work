cfa5c4e97e8f8baf2b96bda1b2cbf4072784ac58
sergey <saizotov81@gmail.com>
2024-01-05 21:50:31 +0500

    Создал класс PackAnimals

---
+++ PackAnimals/PackAnimals.java
@@ -0,0 +1,18 @@
+package PackAnimals;
+import Animals.Animals;
+
+public class PackAnimals extends Animals {
+    private String[] commands;
+    public PackAnimals(String name, String dateOfBirth, String[] commands) {
+        super(name, dateOfBirth);
+        this.commands = commands;
+    }
+    public void displayInfo() {
+        super.displayInfo();
+
+        System.out.println("Commands: ");
+        for (String command : commands) {
+            System.out.println("- " + command);
+        }
+    }
+}
---
+++ Pets/Pets.java
@@ -0,0 +1,20 @@
+package Pets;
+import Animals.Animals;
+
+public class Pets extends Animals {
+    private String[] commands;
+
+    public Pets(String name, String dateOfBirth, String[] commands) {
+        super(name, dateOfBirth);
+        this.commands = commands;
+    }
+
+    public void displayInfo() {
+        super.displayInfo();
+
+        System.out.println("Commands: ");
+        for (String command : commands) {
+            System.out.println("- " + command);
+        }
+    }
+}
