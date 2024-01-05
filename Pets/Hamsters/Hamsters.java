a562e500d5ff19cd8e7471c6cd77f4f2f0980030
sergey <saizotov81@gmail.com>
2024-01-05 22:09:22 +0500

    Создал класс Hamsters

---
+++ Pets/Hamsters/Hamsters.java
@@ -0,0 +1,9 @@
+package Pets.Hamsters;
+
+import Pets.Pets;
+
+public class Hamsters extends Pets {
+    public Hamsters(String name, String dateOfBirth, String[] commands) {
+        super(name, dateOfBirth, commands);
+    }
+}
