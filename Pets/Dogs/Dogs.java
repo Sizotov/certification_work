d1be2d2c5651f38edc6f94a321e080830a5913b3
sergey <saizotov81@gmail.com>
2024-01-05 22:08:08 +0500

    Создал класс Dogs

---
+++ Pets/Dogs/Dogs.java
@@ -0,0 +1,9 @@
+package Pets.Dogs;
+
+import Pets.Pets;
+
+public class Dogs extends Pets {
+    public Dogs(String name, String dateOfBirth, String[] commands) {
+        super(name, dateOfBirth, commands);
+    }
+}
