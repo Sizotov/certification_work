79a77bc8ead400d1210fc4503d02ac2c0f0c8909
sergey <saizotov81@gmail.com>
2024-01-05 22:04:05 +0500

    Создал класс Cats

---
+++ Pets/Cats/Cats.java
@@ -0,0 +1,9 @@
+package Pets.Cats;
+
+import Pets.Pets;
+
+public class Cats extends Pets {
+    public Cats(String name, String dateOfBirth, String[] commands) {
+        super(name, dateOfBirth, commands);
+    }
+}
